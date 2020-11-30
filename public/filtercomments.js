ac6f9704fb90d0ebf6979592cb94676037253b8e
Arcanjo Cassiel <arcanjocassiel687@gmail.com>
2020-11-30 15:35:33 -0300

    creation of the function to loader

--- public/filtercomments.js
+++ public/filtercomments.js
@@ -1,5 +1,5 @@
 const commentscontainer = document.getElementById('comments-container')
-
+const loadercontainer = document.querySelector('.loader')
 let page = 1
 
 const getComments = async() => {
@@ -24,3 +24,37 @@ const addCommentsIntoDOM = async() => {
     commentscontainer.innerHTML += commentsTemplate
 }
 addCommentsIntoDOM()
+
+// loader
+function loader() {
+    const getNextComments = () => {
+        setTimeout(() => {
+            page++
+            addCommentsIntoDOM()
+
+        }, 300)
+    }
+    const removerloader = () => {
+        setTimeout(() => {
+            loadercontainer.classList.remove('show')
+            getNextComments()
+        }, 1000)
+    }
+
+    const shwoloader = () => {
+        loadercontainer.classList.add('show')
+        removerloader()
+    }
+
+    window.addEventListener('scroll', () => {
+        const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+        const isPageBottomAlmostReached = scrollTop + clientHeight >= scrollHeight - 10
+
+        if (isPageBottomAlmostReached) {
+            shwoloader()
+        }
+    })
+}
+loader()
+
+// Filtragem
