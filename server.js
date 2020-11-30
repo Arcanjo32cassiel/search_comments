86167043416c6fe1a14d5372a64fa7436d259c40
Arcanjo Cassiel <arcanjocassiel687@gmail.com>
2020-11-30 11:23:26 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+// server.use(express.static('public'))
+server.use(express.static(__dirname + '/public/'));
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+        express: server
+    })
+    // server.use(express.static(__dirname + "/public", {
+    //     index: false,
+    //     immutable: true,
+    //     cacheControl: true,
+    //     maxAge: "30d"
+    // }));
+server.get("/", function(req, res) {
+    return res.render("index")
+})
+
+server.listen(6800)
